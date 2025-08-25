c0a372f7d20a39885e4e2ad3d2cc350f656f02ab
Craig Hill <64085763+craigtkhill@users.noreply.github.com>
2025-08-25 18:04:10 +0100

    feature: use bat for syntax highlighting of man pages

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
 set -x PATH /opt/homebrew/bin /usr/local/bin $PATH
 starship init fish | source
 zoxide init fish | source
+
+# Use bat for syntax highlighting in man pages
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x MANROFFOPT "-c"
