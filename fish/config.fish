65c82819e46ce7ac810a97b4ed5bb43f7e6fa9c1
Craig Hill <64085763+craigtkhill@users.noreply.github.com>
2025-10-02 21:01:41 +0100

    alias batman to man

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,8 @@ starship init fish | source
 zoxide init fish | source
 
 # Use bat for syntax highlighting in man pages
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-set -x MANROFFOPT "-c"
+# set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x MANROFFOPT -c
+
+# Aliases
+alias man='batman'
