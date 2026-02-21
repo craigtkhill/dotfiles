51bee9c2e64f755674d167633e197e3eb4fd9284
Craig Hill <64085763+craigtkhill@users.noreply.github.com>
2026-02-21 12:42:30 +0000

    feat(fish): add fzf shell integration

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,7 @@
 fish_add_path $HOME/.local/bin
 starship init fish | source
 zoxide init fish | source
+fzf --fish | source
 
 # Use bat for syntax highlighting in man pages
 # set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
