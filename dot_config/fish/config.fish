98c1531207e7c01a6fc6f17c66fd4eea2742ebeb
Craig Hill <64085763+craigtkhill@users.noreply.github.com>
2026-02-23 08:47:38 +0000

    feat(fish): set EDITOR/VISUAL to nvim, enable bat MANPAGER

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,9 +3,10 @@ starship init fish | source
 zoxide init fish | source
 fzf --fish | source
 
-# Use bat for syntax highlighting in man pages
-# set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-set -x MANROFFOPT -c
+# Editor
+set -x EDITOR nvim
+set -x VISUAL nvim
 
-# Aliases
-alias man='batman'
+# Use bat for syntax highlighting in man pages
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x MANROFFOPT -c
