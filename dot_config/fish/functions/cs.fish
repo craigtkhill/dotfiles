d4aa751b857e8f6ec86ae532bd9159f87ee445d9
Craig Hill <64085763+craigtkhill@users.noreply.github.com>
2026-03-17 09:18:40 +0000

    fix(cheatsheets): use correct ~/.cheatsheets path

--- dot_config/fish/functions/cs.fish
+++ dot_config/fish/functions/cs.fish
@@ -1,11 +1,11 @@
 function cs --description "Open a cheatsheet with glow"
-    set cheatsheet ~/cheatsheets/$argv[1].md
+    set cheatsheet ~/.cheatsheets/$argv[1].md
     if test -f $cheatsheet
         glow $cheatsheet
     else
         echo "No cheatsheet for '$argv[1]'"
         echo "Available:"
-        for f in ~/cheatsheets/*.md
+        for f in ~/.cheatsheets/*.md
             echo "  "(basename $f .md)
         end
     end
