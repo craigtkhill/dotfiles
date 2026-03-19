d09f2f93b126fb01a18bc190bf60e6e236565f30
Craig Hill <64085763+craigtkhill@users.noreply.github.com>
2026-03-19 20:16:24 +0000

    chore(ripgrep): add config with smart-case, hidden files, and git exclude

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,6 +4,9 @@ zoxide init fish | source
 fzf --fish | source
 direnv hook fish | source
 
+# Ripgrep
+set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
+
 # Editor
 set -x EDITOR nvim
 set -x VISUAL nvim
